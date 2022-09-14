7404cc0ae02ee39387ef215cd3d398bf5758bd43
Nandini <jnandini668@gmail.com>
2022-09-14 10:14:48 +0530

    product was created in admin page,static backup files are created named as asset,template cutting for dynamic product loading,media image settings for product image saving

--- debonair/debonair/settings.py
+++ debonair/debonair/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+    'product'
 ]
 
 MIDDLEWARE = [
@@ -123,9 +126,14 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
-
+STATIC_ROOT=os.path.join(BASE_DIR,'asset')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#Media files 
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
--- debonair/home/views.py
+++ debonair/home/views.py
@@ -1,42 +1,24 @@
 from email.policy import HTTP
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import product
+from product.models import TravelPlace
 
 
 # Create your views here.
 
 
 
-p1=product()
-p1.name="loreal"
-p1.price=200
-p1.dsc="best hair shampoo"
 
-p2=product()
-p2.name="tresemme"
-p2.price=400
-p2.dsc="have slight sulphate content"
-
-p3=product()
-p3.name="arata"
-p3.price=250
-p3.dsc="curly hair friendly shampoo"
-
-p4=product()
-p4.name="himalaya"
-p4.price=180
-p4.dsc="shampoo for dry skin"
-
-pro=[p1,p2,p3,p4]
 
 #def samp(request):
     
     #return render(request,'test.html',{'l':pro})
 
 def samp(request):
+    data=TravelPlace.objects.all()
     
-    return render(request,'index.html')
+
+    return render(request,'index.html',{'d':data})
 
 def sam(request):
     
